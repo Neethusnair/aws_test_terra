de9cf30b15e0958421602315fcf27f9bf0a63d7b
Neethu <neethu.somasekharan@sreyo.com>
2021-02-26 09:57:18 -0800

    first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region     = "us-west-1"
+  #access_key = "*****"
+  #secret_key = "********"
+}
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-00831fc7c1e3ddc60"
+  instance_type = "t2.micro"
+  tags = {
+      Name = "my-web" 
+
+  }
+
+  
+}
